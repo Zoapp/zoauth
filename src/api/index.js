e7c5783045ab3a875199d445b89909c1cdfcde32
Guilmain Dorian <guilmain.dorian@gmail.com>
2018-01-18 22:52:38 +0100

    Setup Documentation for the project
    Context ?
    We need to have a documentation for the application
    
    What are you trying to achieve or the steps to reproduce ?
    I install JSdoc and start to work on srx/api/index.js
    
    Which result you had ?
    Jsdoc for index.js is done
    
    TODO ?
    Make documentation for the other files

--- src/api/index.js
+++ src/api/index.js
@@ -10,25 +10,47 @@ import express, { Router } from "express";
 import zoauthServer from "../zoauthServer";
 
 // Express inits
+/**
+ * Use "setHeader" to initialise the header.
+ *
+ * @param {*} res The Response.
+ * @param {*} status The Statut who have the header.
+ * @param {*} cors Required for the Cross-Origin.
+ */
 const setHeaders = (res, status, cors) => {
   res.set("Content-Type", "application/json");
   res.set("Access-Control-Allow-Headers", "Content-Type, access_token");
   res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.set("Access-Control-Allow-Origin", cors || "*");
-  res.status(status);
+  res.status(status); // -> Sets the HTTP status for the response.
 };
 
+/**
+ * Use "sendResponse" to set a Response with a Header to the Client.
+ * <br>The fonction set the statut of Header (401 or 200) by testing the data attribute.
+ *
+ * @param {*} data Contain all the data of response.
+ * @param {*} res The Response.
+ * @param {*} ip IP Address for Cross-Origin.
+ */
 const sendResponse = async (data, res, ip) => {
-  const status = data.error ? 401 : 200;
+  const status = data.error ? 401 : 200; // -> Set status according to data.
   setHeaders(res, status, ip);
-  res.json(data);
+  res.json(data); // -> Takes a Response stream and reads it to completion.
 };
 
+/**
+ * "handleAuthFunc" is use in the process of Routing.
+ *
+ * @param {*} req The Request.
+ * @param {*} res The Response.
+ * @param {*} callback The Callback.
+ */
 const handleAuthFunc = async (req, res, callback) => {
-  const params = req.body;
+  const params = req.body; // -> Get the body of Request.
   // logger.info(JSON.stringify(req.body));
-  const data = await callback(params);
-  const result = data.result || data;
+  const data = await callback(params); // -> Await causes execution to pause waiting a Promise.
+  const result = data.result || data; // -> Fill the const with "data" result or brut "data".
   // TODO ip for CORS
   // logger.info("response=", data);
   sendResponse(result, res);
@@ -37,16 +59,17 @@ const handleAuthFunc = async (req, res, callback) => {
 export default (authServer = null, app = null, config = {}) => {
   let needStart = false;
   let a = app;
-  if (!a) {
-    a = express();
-    a.server = http.createServer(a);
+  if (!a) { // -> App don't exist : need to be created and started
+    a = express(); // -> Create an Express application.
+    a.server = http.createServer(a); // -> Create a Server
     a.use(bodyParser.json());
     a.use(bodyParser.urlencoded({ extended: false }));
+    // \-> Middleware function in Express. Parses incoming requests with urlencoded payloads.
     needStart = true;
   }
   a.authServer = authServer || zoauthServer(config);
 
-  const router = Router();
+  const router = Router(); // Get a router instance
   router.get("/", async (req, res) => {
     sendResponse({ auth: "Ok" }, res);
   });
@@ -75,14 +98,14 @@ export default (authServer = null, app = null, config = {}) => {
   router.use(async (req, res, next) => {
     sendResponse({ error: "unknown request" }, res);
   });
-  const defaultEndpoint = "auth";
+  const defaultEndpoint = "auth"; // -> Set the defaut end point
   const api = config.api || { endpoint: defaultEndpoint };
   const endpoint = api.endpoint || defaultEndpoint;
   a.use(endpoint, router);
 
-  if (needStart) {
+  if (needStart) { // -> Start listening is server need to be start
     a.server.listen(process.env.PORT || api.port || 8081);
   }
 
-  return a;
+  return a; // -> Return the App
 };
